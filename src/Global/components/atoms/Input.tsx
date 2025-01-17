e2b4464b8a1b29413b9138d0d50b47b5316745a0
chris abbott <chris.abs96@gmail.com>
2025-01-17 23:18:18 +0000

    feat: updated Input component to have native view only and edit mode

--- src/Global/components/atoms/Input.tsx
+++ src/Global/components/atoms/Input.tsx
@@ -2,16 +2,34 @@ import * as React from 'react'
 
 import { cn } from '@/Global/lib/utils'
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  readOnly?: boolean
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, readOnly, ...props }, ref) => {
+    const inputClassName = cn(
+      'flex h-9 w-full rounded-md border px-3 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+      readOnly
+        ? 'border-transparent bg-contrast-accent text-foreground cursor-default items-center justify-start'
+        : 'border-input bg-transparent text-foreground focus-visible:ring-ring',
+      className,
+    )
+
+    if (readOnly) {
+      return (
+        <div className={cn(inputClassName, 'flex items-center')}>
+          {props.value || <span className="opacity-50">Not specified</span>}
+        </div>
+      )
+    }
+
     return (
       <input
         type={type}
-        className={cn(
-          'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className,
-        )}
+        className={cn(inputClassName, 'py-1')}
         ref={ref}
+        readOnly={readOnly}
         {...props}
       />
     )
