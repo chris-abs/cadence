7919b364882a6ba01b4ef7ce19570caf29a1bc5d
chris abbott <chris.abs96@gmail.com>
2025-02-01 23:54:33 +0000

    chore: nav refactor

--- src/Global/layout/sidebar/AppSidebar.tsx
+++ src/Global/layout/sidebar/AppSidebar.tsx
@@ -1,13 +1,4 @@
 import * as React from 'react'
-import {
-  AudioWaveform,
-  Command,
-  GalleryVerticalEnd,
-  Box,
-  FolderOpen,
-  Package,
-  Tags,
-} from 'lucide-react'
 
 import {
   SidebarCore,
@@ -18,98 +9,17 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
+  navigationConfig,
 } from './sections'
-
-const data = {
-  teams: [
-    {
-      name: 'Storage',
-      logo: GalleryVerticalEnd,
-      plan: 'Collections',
-      disabled: true,
-    },
-    {
-      name: 'Core',
-      logo: AudioWaveform,
-      plan: 'Startup',
-      disabled: true,
-    },
-    {
-      name: 'Corporate',
-      logo: Command,
-      plan: 'Free',
-      disabled: true,
-    },
-  ],
-  navMain: {
-    entities: [
-      {
-        title: 'Dashboard',
-        url: '/',
-        icon: GalleryVerticalEnd,
-      },
-      {
-        title: 'Workspaces',
-        url: '/workspaces',
-        icon: Box,
-      },
-      {
-        title: 'Containers',
-        url: '/containers',
-        icon: FolderOpen,
-        items: [
-          {
-            title: 'List',
-            url: '/containers',
-          },
-          {
-            title: 'Assign',
-            url: '/containers/assign',
-          },
-        ],
-      },
-      {
-        title: 'Items',
-        url: '/items',
-        icon: Package,
-        items: [
-          {
-            title: 'List',
-            url: '/items',
-          },
-          {
-            title: 'Assign',
-            url: '/items/assign',
-          },
-        ],
-      },
-      {
-        title: 'Tags',
-        url: '/tags',
-        icon: Tags,
-        items: [
-          {
-            title: 'List',
-            url: '/tags',
-          },
-          {
-            title: 'Assign',
-            url: '/tags/assign',
-          },
-        ],
-      },
-    ],
-  },
-}
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <TeamSwitcher teams={navigationConfig.teams} />
       </SidebarHeader>
       <SidebarContent>
-        <SidebarCore config={data.navMain.entities} />
+        <SidebarCore config={navigationConfig.navMain.entities} />
       </SidebarContent>
       <SidebarFooter>
         <SidebarUser />
