36be3b760697e7f190d79da0faf58b1b367b376a
chris abbott <chris.abs96@gmail.com>
2025-02-04 00:10:35 +0000

    feat: collections overview responsive section

--- src/Collection/components/molecules/entity/EntityCard.tsx
+++ src/Collection/components/molecules/entity/EntityCard.tsx
@@ -1,7 +1,14 @@
 import { LucideIcon } from 'lucide-react'
 import { Link } from '@tanstack/react-router'
 
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/Global/components/atoms'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+  ScrollArea,
+} from '@/Global/components/atoms'
 import { Muted } from '@/Global/components/molecules'
 import { EntityType } from '@/Collection/types'
 import { EntityEntry } from './EntityEntry'
@@ -15,7 +22,7 @@ interface EntityCardProps {
 
 export function EntityCard({ type, icon: Icon, count, recentItems }: EntityCardProps) {
   return (
-    <Card className="flex flex-col min-h-[420px]">
+    <Card className="flex flex-col h-[475px]">
       <CardHeader>
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
@@ -26,15 +33,17 @@ export function EntityCard({ type, icon: Icon, count, recentItems }: EntityCardP
         </div>
       </CardHeader>
 
-      <CardContent className="flex-grow space-y-1">
-        <div className="grid grid-cols-1 gap-2 xl:grid-cols-2">
-          {recentItems?.map((item) => (
-            <EntityEntry key={item.id} id={item.id} name={item.name} type={type} icon={Icon} />
-          ))}
-        </div>
-      </CardContent>
+      <ScrollArea className="flex-1">
+        <CardContent className="space-y-1">
+          <div className="grid grid-cols-1 gap-2 xl:grid-cols-2">
+            {recentItems?.map((item) => (
+              <EntityEntry key={item.id} id={item.id} name={item.name} type={type} icon={Icon} />
+            ))}
+          </div>
+        </CardContent>
+      </ScrollArea>
 
-      <CardFooter className="justify-end mt-auto">
+      <CardFooter className="justify-end align-middle pt-4">
         <Link to={`/${type}s`}>
           <Muted className="hover:text-foreground">View all {type}s →</Muted>
         </Link>
