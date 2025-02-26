547641fe862cf7fe5651b83e12d371dc383a0aa3
chris abbott <chris.abs96@gmail.com>
2025-02-26 18:56:40 +0000

    feat: added new aliases

--- src/User/hooks/useUserWithFamily.ts
+++ src/User/hooks/useUserWithFamily.ts
@@ -5,9 +5,6 @@ export function useUserWithFamily() {
   const { data: user, isLoading: isUserLoading } = useUser()
   const { data: family, isLoading: isFamilyLoading } = useFamily(user?.familyId ?? 0)
 
-  console.log('User data:', user)
-  console.log('Family data:', family)
-
   return {
     user,
     family,
