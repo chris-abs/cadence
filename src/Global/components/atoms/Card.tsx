7752065207fb5f4cca6257e5fd9a6625562fc15f
chris abbott <chris.abs96@gmail.com>
2025-03-25 23:23:42 +0000

    chore: card tweaks

--- src/Global/components/atoms/Card.tsx
+++ src/Global/components/atoms/Card.tsx
@@ -25,7 +25,7 @@ Card.displayName = 'Card'
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('flex flex-col space-y-1.5', className)} {...props} />
+    <div ref={ref} className={cn('flex flex-col space-y-1.5 ', className)} {...props} />
   ),
 )
 CardHeader.displayName = 'CardHeader'
@@ -49,9 +49,7 @@ const CardDescription = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HT
 CardDescription.displayName = 'CardDescription'
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
-  ),
+  ({ className, ...props }, ref) => <div ref={ref} className={cn('p-4 ', className)} {...props} />,
 )
 CardContent.displayName = 'CardContent'
 
